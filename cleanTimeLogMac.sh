d7df7aa0811050f7904212c6c39eec749617990b
Walter <walterxie@users.noreply.github.com>
2016-03-30 10:54:35 +1300

    fix clean time log script

--- cleanTimeLogMac.sh
+++ cleanTimeLogMac.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-for f in {"singleThread", "doubleThread", "fourThread", "GPU"}; do
+folders=( "singleThread" "doubleThread" "fourThread" "GPU" )
+for f in "${folders[@]}"; do
   cd $f
   sed -i.bak '/.*Fail.*/d;/.*line.*/d;/.*ile.*/d;/.*WARN.*/d;/.*Igno.*/d;/.*With.*/d;/.*Using.*/d;/.*End.*/d;/^\s*$/d' time.txt
+  echo "clean time.txt in" $f
   cd ..
 done
